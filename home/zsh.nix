b7ad61108feb59b620901f43b22305619d502920
Philipp Dargel <philipp.dargel@neuland.de>
2021-02-24 19:25:56 +0100

    use my theme

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,6 +1,18 @@
 { config, pkgs, ... }:
-
-{
+let
+  agnoster-nix-theme = builtins.fetchurl {
+    url = "https://gist.githubusercontent.com/chisui/0d12bd51a5fd8e6bb52e6e6a43d31d5e/raw/f54d848ba646ba141ec01e19095144d34f326b55/agnoster-nix.zsh-theme";
+    sha256 = "0phkksni37n8jml3hcgkzwihqbpwya6s9bzqy2a9v418sh8ls3sk";
+  };
+  customDir = pkgs.stdenv.mkDerivation {
+    name = "oh-my-zsh-custom-dir";
+    phases = [ "buildPhase" ];
+    buildPhase = ''
+      mkdir -p $out/themes
+      cp ${agnoster-nix-theme} $out/themes/agnoster-nix.zsh-theme  
+    '';
+  };
+in {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -19,6 +31,8 @@
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
+      theme = "agnoster-nix";
+      custom = "${customDir}";
     };
   };
 }
