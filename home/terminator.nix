0c087d7a45c9cc32bff553ce9e4deb2cbd7c4012
Philipp Dargel <chisui.pd@gmail.com>
2021-06-01 01:16:42 +0200

    unified terminator font setting

--- home/terminator.nix
+++ home/terminator.nix
@@ -1,5 +1,7 @@
 { config, pkgs, ... }:
-{
+let
+  font = "Droid Sans Mono for Powerline 10";
+in {
   home = {
     packages = [ pkgs.terminator ];
     file.".config/terminator/config".text = ''
@@ -7,12 +9,12 @@
         focus = mouse
         borderless = True
         title_use_system_font = False
-        title_font = Droid Sans Mono for Powerline 9
+        title_font = ${font}
       [keybindings]
       [profiles]
         [[default]]
           cursor_color = "#aaaaaa"
-          font = Droid Sans Mono for Powerline 10
+          font = ${font}
           show_titlebar = False
           palette = "#073642:#dc322f:#859900:#b58900:#268bd2:#d33682:#2aa198:#eee8d5:#002b36:#cb4b16:#586e75:#657b83:#839496:#6c71c4:#93a1a1:#fdf6e3"
           use_system_font = False
