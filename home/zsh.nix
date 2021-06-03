8cdab01ad470e2198b0284c3a3a9cbbad68281e4
Philipp Dargel <chisui.pd@gmail.com>
2021-06-03 02:11:20 +0200

    zsh cleanup

--- home/zsh.nix
+++ home/zsh.nix
@@ -12,22 +12,26 @@ let
       cp ${agnoster-nix-theme} $out/themes/agnoster-nix.zsh-theme  
     '';
   };
+  zsh-nix-shell = {
+    name = "zsh-nix-shell";
+    file = "nix-shell.plugin.zsh";
+    src = pkgs.fetchFromGitHub {
+      owner = "chisui";
+      repo = "zsh-nix-shell";
+      rev = "v0.1.0";
+      sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
+    };
+  };
 in {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     plugins = [
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.1.0";
-          sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
-        };
-      }
+      zsh-nix-shell
     ];
+    shellAliases = {
+      code = "codium";
+    };
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
