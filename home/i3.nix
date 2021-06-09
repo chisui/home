250b9f3e8f8a4b31feea611f93540c084ba01f80
Philipp Dargel <chisui.pd@gmail.com>
2021-06-09 19:15:18 +0200

    use brightnessctl for backlight

--- home/i3.nix
+++ home/i3.nix
@@ -7,15 +7,19 @@
       modifier = "Mod4"; # SuperKey
       keybindings = let
         volumeStep = "5";
+        brightnessStep = "5%";
+        exec = cmd: "exec --no-startup-id ${cmd}";
+        brightnessctl = op: exec "${pkgs.brightnessctl}/bin/brightnessctl set ${brightnessStep}${op}";
         in lib.mkOptionDefault {
-          "${modifier}+Return" = "exec ${pkgs.terminator}/bin/terminator";
-          "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume 0 +${volumeStep}%";
-          "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume 0 -${volumeStep}%";
-          "XF86AudioMute"        = "exec --no-startup-id pactl set-sink-mute 0 toggle";
-          "XF86AudioMicMute"     = "exec --no-startup-id pactl set-source-mute 1 toggle";
+          "${modifier}+Return" = exec "${pkgs.terminator}/bin/terminator";
 
-          "XF86MonBrightnessUp"   = "exec --no-startup-id xrandr --output eDP-1 --brightness 1";
-          "XF86MonBrightnessDown" = "exec --no-startup-id xrandr --output eDP-1 --brightness .1"; 
+          "XF86AudioRaiseVolume" = exec "pactl set-sink-volume 0 +${volumeStep}%";
+          "XF86AudioLowerVolume" = exec "pactl set-sink-volume 0 -${volumeStep}%";
+          "XF86AudioMute"        = exec "pactl set-sink-mute 0 toggle";
+          "XF86AudioMicMute"     = exec "pactl set-source-mute 1 toggle";
+
+          "XF86MonBrightnessUp"   = brightnessctl "+";
+          "XF86MonBrightnessDown" = brightnessctl "-";
         };
     };
   };
