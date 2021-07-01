5a3486e94d482f6a5c8cbb353398a3d8b4909b7f
Philipp Dargel <chisui.pd@gmail.com>
2021-07-01 21:15:16 +0200

    vscodium use vscode module

--- home/vscodium.nix
+++ home/vscodium.nix
@@ -1,13 +1,31 @@
 { config, pkgs, ... }:
 let
-  code-pegjs-language = pkgs.fetchgit {
-    url = "https://github.com/SrTobi/code-pegjs-language.git";
-    rev = "2bf85d6414b5c9607d50335ede567f7505e9fe23";
-    sha256 = "0m7ayc9kbk9yyrq341kai3vk20jrzwq900n1ifxy8s2w0y5qwbdx";
+  ext-pegjs = pkgs.stdenv.mkDerivation {
+    name = "vscode-ext-pegjs-lang";
+    src = pkgs.fetchgit {
+      url = "https://github.com/SrTobi/code-pegjs-language.git";
+      rev = "2bf85d6414b5c9607d50335ede567f7505e9fe23";
+      sha256 = "0m7ayc9kbk9yyrq341kai3vk20jrzwq900n1ifxy8s2w0y5qwbdx";
+    };
+    installPhase = ''
+      mkdir -p $out/share/vscode/extensions/pegjs-language
+      cp -r $src/* $out/share/vscode/extensions/pegjs-language
+    '';
   };
+  
 in {
-  home = {
-    packages = [ pkgs.vscodium ];
-    file.".vscode-oss/extensions/code-pegjs-language".source = code-pegjs-language;
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    userSettings = {
+      "rust-client.disableRustup" = true;
+      "workbench.editor.untitled.hint" = "hidden";
+      "terminal.integrated.fontFamily" = "Droid Sans Mono for Powerline";
+      "workbench.startupEditor" = "none";
+      "workbench.editor.enablePreview" = false;
+    };
+    extensions = [
+      ext-pegjs
+    ];
   };
 }
